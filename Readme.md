260739345e9c789e02a08224be5e589c81e21dd4
David R. Jenni <david.r.jenni@gmail.com>
2015-01-15 23:32:53 +0100

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# agodoc
+
+agodoc - godoc for Acme.
+
+## Installation
+
+```
+% go get github.com/davidrjenni/agodoc
+
+```
---
+++ agodoc_test.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2014 David R. Jenni. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+type offsetTest struct {
+	data       []byte
+	offset     int
+	byteOffset int
+}
+
+var offsetTests = []offsetTest{
+	offsetTest{[]byte("abcdef"), 0, 0},
+	offsetTest{[]byte("abcdef"), 1, 1},
+	offsetTest{[]byte("abcdef"), 5, 5},
+	offsetTest{[]byte("日本語def"), 0, 0},
+	offsetTest{[]byte("日本語def"), 1, 3},
+	offsetTest{[]byte("日本語def"), 5, 11},
+}
+
+func TestByteOffset(t *testing.T) {
+	for _, test := range offsetTests {
+		off, err := byteOffset(bytes.NewReader(test.data), test.offset)
+		if err != nil {
+			t.Errorf("got error %v", err)
+		}
+		if off != test.byteOffset {
+			t.Errorf("expected byte offset %d, got %d", test.byteOffset, off)
+		}
+	}
+}
